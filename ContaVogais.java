63873e93b60bbf4212a36e41a26495a151ec268e
Gustavo Porto <105179924+gustavoapc@users.noreply.github.com>
2022-07-21 15:17:18 -0300

    Add files via upload

---
+++ ContaVogais.java
@@ -0,0 +1,41 @@
+package contador;
+import java.util.Scanner;
+
+public class ContaVogais {
+	
+	public static int contador(String str) {
+		
+		char v;
+		
+		int vowels=0;
+		
+		for(int i=0;i<str.length();i++) {
+			v=str.charAt(i);
+			 
+			switch(v) {
+			case 'a':
+			case 'e':
+			case 'i':
+			case 'o':
+			case 'u':
+				vowels++;
+				break;
+				
+			}
+			}
+		return vowels;
+	}
+	
+		
+	
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		System.out.println("digite uma string");
+		String str= scan.nextLine();
+		
+		System.out.println("String: ("+str+") = " + contador(str));
+	}
+		// TODO Auto-generated method stub
+
+	
+}
